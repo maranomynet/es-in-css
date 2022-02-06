812c8620b0af3acf4e1e060c2fb9a0d8a45336b1
mar <mar.nospam@anomy.net>
2022-02-06 12:22:11 +0000

    chore: create empty project

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = require('@hugsmidjan/hxmstyle')({
+  // Place your project-specific additions or overrides here
+  // using standard ESLint config syntax...
+
+  parserOptions: {
+    // ecmaVersion: 9, // fantasy
+  },
+});
