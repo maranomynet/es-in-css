f3e304e3c3b065db45740ee2d4ff1322d8077384
mar <mar.nospam@anomy.net>
2022-02-09 22:18:33 +0000

    feat: Implement `VariableData<T>.override()`

--- src/variables.ts
+++ src/variables.ts
@@ -1,20 +1,30 @@
 import { Unit } from './units';
 
+const mapObject = <T extends string, V>(
+  input: Record<T, unknown>,
+  mapper: (item: T) => V
+): Record<T, V> =>
+  Object.fromEntries(
+    (Object.keys(input) as Array<T>).map((name) => [name, mapper(name)])
+  ) as Record<T, V>;
+
+const makeDeclarations = (vars: Record<string, VariableValue>): string =>
+  Object.keys(vars)
+    .map((name) => `--${name}: ${vars[name]};\n`)
+    .join('');
+
+export type VariableValue = string | Unit;
+
 export type VariableStyles<T extends string> = {
   readonly declarations: string;
   readonly vars: Record<T, string>;
+  override<K extends T>(vars: Record<K, string>): string;
 };
 
 export const variables = <T extends string>(
-  input: Record<T, string | Unit<unknown>>
-): VariableStyles<T> => {
-  const ret = {
-    declarations: '',
-    vars: {} as Record<T, string>,
-  };
-  (Object.keys(input) as Array<T>).forEach((name) => {
-    ret.declarations += `--${name}: ${input[name]};\n`;
-    ret.vars[name] = `var(--${name})`;
-  });
-  return ret as VariableStyles<T>;
-};
+  input: Record<T, VariableValue>
+): VariableStyles<T> => ({
+  declarations: makeDeclarations(input),
+  vars: mapObject(input, (name) => `var(--${name})`),
+  override: makeDeclarations,
+});
