56b9dd0a4435e834765916bdd9171f222e2e861d
Már Örlygsson <mar.nospam@anomy.net>
2022-02-21 12:33:17 +0000

    fix: Patch color's lozzy hex method
    See: https://github.com/Qix-/color/issues/243

--- src/colors.ts
+++ src/colors.ts
@@ -14,6 +14,18 @@ export const color = Color as ColorPlus;
 export const rgb = color.rgb;
 export const hsl = color.hsl;
 
+// Patch color's lozzy hex method.
+// See: https://github.com/Qix-/color/issues/243
+const oldHex = (color.prototype as Color).hex;
+(color.prototype as Color).hex = function (value) {
+  if (arguments.length > 0) {
+    return new Color(value);
+  }
+  // @ts-expect-error  (@types/color don't fully match color's actual implementation)
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  return this.valpha === 1 ? oldHex.call(this) : this.hexa();
+} as Color['hex'];
+
 /**
  * Type-safe name to color mapper. Alias for `color(colorName)`
  *
